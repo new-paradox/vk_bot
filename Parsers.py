9047fbfea23773c0baff9a5f61662c0fe90847e0
Evgeniy Ryabenkov <irksunbeam@gmail.com>
2020-10-24 12:55:08 +0800

    add docstring

--- Parsers.py
+++ Parsers.py
@@ -17,21 +17,35 @@ NEWS_WORLD = 'span', \
 
 
 class Parser:
+    """
+    Парсит сайт Информационного агентсва ТАСС
+    Отправляет результат парсинга в get_scenario
+    """
+
     def __init__(self, location):
         self.location = location
         self.table_news = list()
 
     def run_parse(self):
+        """
+        запускает парсинг на оснвоании локации "ru" or "world";
+        :return: <list> [новость1, новость2,..]
+        """
         if self.location == 'ru':
             return self.parse_news(html=HTML_RUSSIA.text, news_local=NEWS_RUSSIA)
         elif self.location == 'world':
             return self.parse_news(html=HTML_WORLD.text, news_local=NEWS_WORLD)
 
     def parse_news(self, html, news_local):
+        """
+        :param html: BeautifulSoup obj
+        :param news_local: 'ru' or 'world'
+        :return: <list> [новость1, новость2,..]
+        Парсинг сайтов;
+        """
         soup = BeautifulSoup(html, 'html.parser')
         news = soup.findAll(news_local)
         news = list(set(news))
         if len(news) > 30:
             news = news[:(30 - len(news))]
         return [news_.text for index, news_ in enumerate(news) if 18 < len(news_.text) < 130]
-
--- bot.py
+++ bot.py
@@ -13,14 +13,24 @@ except ImportError:
 
 class Bot:
     """
-    echo bot for VK API
+    Bot for VK API
     use python3.8
+
+    Бот всегда слушает, когда поступает сообщение
+    от пользователя, пользователь заносится в users;
+    Бот начинает обрабатывать событие - on_event, если событие в сценарии - сценарий начинает
+    работать - get_scenario;
+    сценарий содержит в себе варианты запуска парсера сайта информмционного агентсва ТАСС
+    по соответсвующим критериям (новости России или мира), полученный ответ возвращается пользователю -
+    message_send;
+
     """
 
     def __init__(self, id_group, token_):
         """
         :param id_group: group id из vk
         :param token_: секретный токен
+
         """
         self.id_group = id_group
         self.vk = vk_api.VkApi(token=token_)
@@ -41,35 +51,51 @@ class Bot:
 
     def on_event(self, event):
         """
-        обработка сообщений
+        обработка поступающих сообщений;
+        Если это тектовое сообщение -> начинается его обработка
+        Если пользователя нет в users, бот здоровается с пользователем;
+        И добавляет его в users;
+        Если пользователь уже есть в users -> начинается отработка сценария;
+        Если сообщение от пользователя подходит по ключам к сцению -> запускается
+        соотвествующий сценарий;
+        Иначе пользователь получает DEFAULT_ANSWER.
         """
         if event.type == VkBotEventType.MESSAGE_NEW:
             user_id = event.object.message['from_id']
             text = event.object.message['text'].lower()
             print(text)
-
+            # приветствие
             if user_id not in self.users:
-                # приветствую нового пользователя
                 self.users[user_id] = user_id
                 self.message_send(text_to_send=config.INTENTS[0]['answer'], user_id=user_id)
-            else:
-                # обработка сценария
+            else:  # сценарий
                 for intent in config.INTENTS:
                     if text in intent['tokens']:
                         print('есть нужный ответ')  # TODO: добавить логирование
-                        text_to_send = self.start_scenario(scenario_name=intent['scenario'])
+                        text_to_send = self.get_scenario(scenario_name=intent['scenario'])
                         self.message_send(text_to_send=text_to_send, user_id=user_id)
                         break
                 else:
                     self.message_send(text_to_send=config.DEFAULT_ANSWER, user_id=user_id)
 
     def message_send(self, text_to_send, user_id):
+        """
+        :param text_to_send: <str> ответ пользователю
+        :param user_id: id пользователя в вк
+        Отправка ответа пользователю;
+        """
         self.api.messages.send(
             message=text_to_send,
             random_id=0,
             user_id=user_id)
 
-    def start_scenario(self, scenario_name):
+    def get_scenario(self, scenario_name):
+        """
+        :param scenario_name: <str> "ru" or "world"
+        :return: <str> ответ пользователю
+        Обработка сценария;
+        Парсинг сайтов - Parser;
+        """
         scenario = config.SCENARIOS[scenario_name]
         try:
             text_to_send = scenario['text']
